a46cb806f34b0ba0448a40d7b01fb9a781b1634a
Miguel Alonso <mgl.suarez1@gmail.com>
2021-04-25 12:03:09 -0500

    lskshs

--- apps/user/forms.py
+++ apps/user/forms.py
@@ -5,11 +5,11 @@ class FormLogin(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(FormLogin, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['id'] = 'username'
-        self.fields['username'].widget.attrs['class'] = 'form-control form-control-user font-weight-bold text-primary'
+        self.fields['username'].widget.attrs['class'] = 'form-control form-control-user text-primary'
         self.fields['username'].widget.attrs['autocomplete'] = 'off'
         self.fields['username'].widget.attrs['placeholder'] = 'Usuario'
         self.fields['username'].widget.attrs['style'] = 'font-size: 16px;'
         self.fields['password'].widget.attrs['id'] = 'password'
-        self.fields['password'].widget.attrs['class'] = 'form-control form-control-user font-weight-bold text-primary'
+        self.fields['password'].widget.attrs['class'] = 'form-control form-control-user text-primary'
         self.fields['password'].widget.attrs['placeholder'] = 'Contraseña'
         self.fields['password'].widget.attrs['style'] = 'font-size: 16px;'
